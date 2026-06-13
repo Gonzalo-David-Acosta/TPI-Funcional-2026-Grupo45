f49a28e0d4d4beafd2ab2dec74f27825aa49c9c9
Gonzalo-David-Acosta <gonzalodavidacosta46@gmail.com>
2026-06-13 03:26:25 -0300

    Requerimiento 4: Implementación de duracion-ciclo y recomendacion-ciclo.

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -7,3 +7,30 @@
 (defun informe (datos)
 	(format t "Tiempo ~A: la luz ha cambiado de ~A a ~A ~%" (first datos) (second datos) (third datos))
 )
+
+;; ========================================================
+;; FUNCIÓN: duracion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun duracion-ciclo (ciclo)
+	(and (consp ciclo) (numberp (first ciclo)) (numberp (second ciclo)) (numberp (third ciclo))
+		(+ (first ciclo) (second ciclo) (third ciclo))
+	)
+)
+
+;; ========================================================
+;; FUNCIÓN: recomendacion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun recomendacion-ciclo (duracion)
+	(cond
+		((NULL duracion) "Error: No se puede dar una recomendacion con una duracion invalida")
+		((< duracion 35) "Ciclo demasiado corto")
+		((> duracion 150) "Ciclo demasiado largo")
+		(t "Ciclo optimo")
+	)
+)
